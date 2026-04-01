29f29e6ea883051a875bd7c277e70be0f620c013
LeeJeongHun1216 <as01090618260@gmail.com>
2026-04-01 23:53:28 +0900

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -2,6 +2,6 @@
 // - 카카오맵 키는 index.html의 SDK 스크립트 URL에 appkey=... 로 넣습니다.
 // - 백엔드는 기본값 http://127.0.0.1:5179 입니다.
 window.APP_CONFIG = {
-  BACKEND_BASE_URL: "https://public-bike-api.onrender.com"
+  BACKEND_BASE_URL: "https://public-bike-web.onrender.com"
 };
 
